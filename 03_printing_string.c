d9518b2913c325df4d4527e414442605274474f2
Pradeep Yadav <pradeepyadav4614838@gmail.com>
2025-01-23 13:23:30 +0530

    Add files via upload

---
+++ 03_printing_string.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (){  
+    char st[] = "abc"; 
+    printf("%s\n", st); // %s is used to print string 
+    return 0;
+}
---
+++ 06_string_function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+ char st[] = "pradeep";
+
+    printf("Length of string is %d\n", strlen(st));
+    return 0;
+}
+
